3e3a03ad607cde7d6586f6c593a5966148a27c8d
SuperNova74-k <kingkhaled.w@gmail.com>
2025-05-04 19:31:43 +0300

    utils read write file

--- utils/files.py
+++ utils/files.py
@@ -1,6 +1,6 @@
 def read_file(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
         return content
     except FileNotFoundError:
