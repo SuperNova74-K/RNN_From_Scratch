7cd2255b662d4632268bab9fcfb9347b378b5274
SuperNova74-k <kingkhaled.w@gmail.com>
2025-05-07 18:55:24 +0300

    before M fixing

--- src/RNN.py
+++ src/RNN.py
@@ -49,14 +49,14 @@ class RNN():
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        scale = 0.01
+        scale_of_weights_init = 0.01
 
-        self.wxh = torch.rand(input_size, hidden_size, requires_grad=True).to(self.device)  * scale
-        self.whh = torch.rand(hidden_size, hidden_size, requires_grad=True).to(self.device) * scale
-        self.why = torch.rand(hidden_size, output_size, requires_grad=True).to(self.device) * scale
+        self.wxh = torch.rand(input_size, hidden_size, requires_grad=True , device=self.device) * scale_of_weights_init
+        self.whh = torch.rand(hidden_size, hidden_size, requires_grad=True, device=self.device) * scale_of_weights_init
+        self.who = torch.rand(hidden_size, output_size, requires_grad=True, device=self.device) * scale_of_weights_init
 
-        self.bh = torch.rand(hidden_size, requires_grad=True).to(self.device)
-        self.hy = torch.rand(output_size, requires_grad=True).to(self.device)
+        self.bh = torch.rand(hidden_size, requires_grad=True, device=self.device)
+        self.bo = torch.rand(output_size, requires_grad=True, device=self.device)
 
         self.index_to_token = index_to_token
         self.token_to_index = token_to_index
@@ -65,135 +65,200 @@ class RNN():
         self.one_hot = one_hot
         self.eos = self.token_to_index[eos]
 
+        self.learning_rate = torch.tensor(0.001)
+        self.parameters = [self.wxh, self.whh, self.who, self.bh, self.bo]
+        self.optimizer = torch.optim.AdamW(self.parameters, self.learning_rate)
+
     def embed(self, index, one_hot=None):
-        if one_hot is None: 
-            one_hot = self.one_hot
-
+        if one_hot is None:
+            one_hot = True
+        
         if one_hot:
-            vector = torch.zeros(self.input_size, device=self.device)
-            vector[index] = 1.0
-            return vector
+            embedding = torch.zeros(self.input_size, device=self.device)
+            embedding[index] = 1.0
+            return embedding
 
-        return self.index_to_embedding[index].to(self.device)
+        return torch.tensor(self.index_to_embedding[index], device=self.device)
+            
 
     def forward(self, x_t, hidden_state):
+        # (B, H)
         new_hidden_state = torch.tanh(
-            x_t @ self.wxh + 
-            hidden_state @ self.whh    +
+            x_t @ self.wxh +
+            hidden_state @ self.whh + 
             self.bh
-        ).to(self.device)
+        )
 
-        output_logits = (new_hidden_state @ self.why + self.hy).to(self.device)
+        # (B, O)
+        output = new_hidden_state @ self.who + self.bo
 
-        return output_logits, new_hidden_state
-    
-    # TODO: gpu the heck out of it
+        return new_hidden_state, output
+
     def sequence_to_hidden(self, sequence, hidden_state=None):
-        if hidden_state is None:
-            hidden_state = self.get_fresh_hidden_state().to(self.device)
-
         with torch.no_grad():
-            for token_index in sequence:
-                token = self.embed(token_index).to(self.device)
-                output, hidden_state = self.forward(token, hidden_state)
-            
+            hidden_state = self.get_fresh_hidden_state(batch_size=1)
+
+            for i in range(len(sequence)):
+                # (B, I)
+                x = torch.zeros(1, self.input_size)
+                
+                x[0] = self.embed(sequence[i])
+
+                # (B, H)      (B, O)
+                hidden_state, output = self.forward(x, hidden_state)
+
             return hidden_state
-
-    def get_fresh_hidden_state(self, n_batches):
-        return torch.zeros(n_batches, self.hidden_size, requires_grad=True).to(self.device)
-    
-    def sample(self, sequence, max_length=2000, deterministic=False):
-        generated = list(sequence)
-        hidden_state = self.sequence_to_hidden(generated)
-        
-        for _ in range(max_length):
-            with torch.no_grad():
-                last_idx = generated[-1]
-                token = self.embed(last_idx)
-                output_logits, hidden_state = self.forward(token, hidden_state)
-                probabilities = torch.softmax(output_logits, dim=0)
-                
-                if deterministic:
-                    output_idx = torch.argmax(probabilities).item()
-                else:
-                    output_idx = torch.multinomial(probabilities, 1).item()
-                
-                if output_idx == self.eos_index:
-                    break
-
-                generated.append(output_idx)
-                
-
-        output_sequence = [self.index_to_token[idx] for idx in generated]
-        return "".join(output_sequence)
-        
-    
-    def train(self, sequences, epochs, checkpoints=[], batch_size=1, learning_rate=0.01, max_grad_norm=5.0):
-        for epoch in tqdm(range(epochs), desc="Epoch", position=0):
-            epoch_loss = 0
             
-            for sequence in tqdm(sequences, desc="sequence", position=1, leave=False):
+    
+    def get_fresh_hidden_state(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size, device=self.device)
+    
+    def generate(self, sequence, max_length=100, deterministic=False):
+        with torch.no_grad():
+            generated_sequence = []
+
+            hidden_state = self.get_fresh_hidden_state(batch_size=1)
+            hidden_state = self.sequence_to_hidden(sequence, hidden_state)
+
+            last_token_index = sequence[-1]
+
+            for i in tqdm(range(max_length), desc="Tokens Generated:"):
+                # (B, I)
+                x = torch.zeros(1, self.input_size)
                 
-                hidden_state = self.get_fresh_hidden_state(n_batches=batch_size)
-                
-                sequence_loss = 0
+                x[0] = self.embed(last_token_index)
 
-                subsequence_length = len(20)
-                subsequences_start_indexes = range(0, len(sequence) - subsequence_length + 1, subsequence_length)
-                subsequences = [sequence[i][i + subsequence_length] for i in subsequences_start_indexes]
-                for sequence_index, subsequence in enumerate(subsequences):
-                    X = torch.zeros(batch_size, subsequence_length, self.input_size, device=self.device)
+                # (B, H)      (B, O)
+                hidden_state, output = self.forward(x, hidden_state)
 
-                    for token_index in range(subsequence_length):
-                        
-                        # current_token_index_in_subsequence = sub_sequences_start_index + token_index
-                        # next_token_index_in_subsequence = current_token_index_in_subsequence + 1
+                probabilities = torch.softmax(output[0])
 
-                        # if next_token_index_in_subsequence >= len(sequence):
-                        #     break
+                output_token_index = torch.argmax(probabilities).item()
 
-                        # current_token_global_onehot_index = sequence[current_token_index_in_subsequence]
-                        # next_token_global_onehot_index = sequence[next_token_index_in_subsequence]
+                if not deterministic:
+                    output_token_index = torch.multinomial(probabilities, num_samples=1).item()
 
-                        X[sequence_index, token_index, token] = 1.0
-                        
+                generated_sequence.append(output_token_index)
 
-                        for t in range(subsequence_length):
-                            # ()
-                            x_t = X[t]
-                            
-                            # (B, I) ,,,,,, (B, H)
-                            output_logits, hidden_state = self.forward(x_t, hidden_state)
-                            
-                            # (B, I)
-                            probabilities = torch.softmax(output_logits, dim=1)
+                last_token_index = output_token_index
 
-                            loss = torch.mean(
-                                -torch.log(
-                                    probabilities[] + 1e-9
-                                )
-                            )
-
-
-
-                    # Forward pass through entire sequence
-                    for i in range(len(sequence) - 1):
-                        token = self.embed(sequence[i])
-                        output_logits, hidden_state = self.forward(token, hidden_state)
-                        probabilities = torch.softmax(output_logits, dim=0)
-                        target_index = sequence[i + 1]
-                        
-                        # Accumulate loss
-                        sequence_loss += -torch.log(probabilities[target_index] + 1e-9)
-
-                epoch_loss += sequence_loss.item()
-
-            # Epoch statistics
-            avg_loss = epoch_loss / len(sequences)
-            print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss:.4f}")
-
-            if (epoch + 1) in checkpoints:
-                print(f"checkpoint at epoch {epoch + 1}")
-                checkpoints.append(self)
+                if last_token_index == self.eos:
+                    break
+            
+            return ''.join([self.index_to_token(index) for index in generated_sequence])
         
-        return checkpoints.append(self)
+    
+    def train(self, sequences, epochs, checkpoints=[], batch_size=1, sub_sequence_length=None, max_grad_norm=5.0):
+        models = []
+        for epoch in tqdm(range(epochs), desc="Epochs Finished: ", position=0):
+            epoch_loss = 0
+            for sequence in tqdm(sequences, desc="Sequences Finished:", position=1, leave=False):
+                # I swear these comments and code are written by a human (me), no AI involved here.
+                
+                # --- getting sub-sequences ready for training ---
+                # sequence is like [0, 52, 34, 60]
+                # sub-sequences will be like (batch size equal 2 which is how many rows we have):
+                    # [
+                    #   [0, 52],
+                    #   [34, 60],
+                    #] ... so if sub_sequence_length is not explicitly provided, we just infer it, if it's explicitly provided then we use it and do many batches till the sub-sequences are all finished
+                
+                # TODO: Prime factorization if batch_size is None to use as much of the training data as possible
+
+                if sub_sequence_length is None:
+                    sub_sequence_length = len(sequence) // batch_size
+
+                # the point of the following is to create subsequences with the previously defined length but to ensure they all have the same size, ignore the reminder of the main sequence if you have to
+                # if sequence is [34, 66, 77, 99, 55] and length is 2 then: 
+                stop_range = len(sequence) - sub_sequence_length # this will be 5 - 2 = 3
+                sub_sequences_start_indexes = list(range(0, stop_range, sub_sequence_length)) # this will be [0, 2]
+                
+                # TODO: this can be optimized for memroy and time but I'm trying to prevent premature optimization
+                sub_sequences = [sequence[start_index: start_index + sub_sequence_length] for start_index in sub_sequences_start_indexes]
+                # this will be [[34, 66], [77, 99]]
+                # notice how the element 55 was discarded! because the sequence doesn't have enough tokens.
+
+                # --- hidden state init ---
+                # (B, H) shape, basically an (H) shape vector for every sub_sequence :)
+                hidden_state = self.get_fresh_hidden_state(batch_size=batch_size)
+
+                M = len(sub_sequences)
+                
+                # --- getting training X and Y ready ---
+                # Recall how batch_size is simply how many sub-sequences to train at once before calculating loss and gradients and so on
+
+                # every column is a subsequence of elements like [33, 66], but because 33 needs to be turned into onehot / embedding, we have to account for that by shape I
+                # every row is a time t of the RNN, so we roll it row by row .. for example:
+
+                '''
+                    x = 
+                    [
+                        [embedding_of_34, embedding_of_77],
+                        [embedding_of_66, embedding_of_99]
+                    ]
+                    
+                    notice that the embedding of an element can be it's onehot or it's embedding vector depending on your choice / context (and in both cases that embedding is also a vector, hence 3d matrix)
+                    also notice that we process this row by row, where each column has the current element of that column's sub-sequence
+                    we have as many columns as batch size because that's how many subsequences we have
+                    we have as many rows as sub-sequence's length because that's how many stpes we have in the training which is also the number of sub-sequence length
+                '''
+                # (S, B, I)
+                X = torch.zeros(sub_sequence_length, batch_size, self.input_size, device=self.device)
+
+                '''
+                TODO: fix this description
+                    Y is an output onehot vector representing logits of next token prediction ... one for every subsequence we are working with
+                    note that every column in Y will have only 1 element with value 1, the rest will be zero because it's a onehot encoding for the ground truth of the prediction
+                    might seem weird but you'll understand when you see the loss hehe :)
+
+                    also note that we could have made it of shape (S, B, O) but will make things more complicated for no reason, just keep going in the code and you'll get it :)
+                '''
+                Y = torch.zeros(sub_sequence_length, batch_size, self.output_size, device=self.device)
+
+                # populating x, y with training data
+                for sub_sequence_index, sub_sequence in enumerate(sub_sequences):
+                    for time_step, element in enumerate(sub_sequence):
+                        X[time_step, sub_sequence_index] = self.embed(element)
+                        
+                        if time_step != 0:
+                            Y[time_step - 1][sub_sequence_index] = self.embed(element, one_hot=True)
+
+
+                # --- training steps ---
+                for i in range(sub_sequence_length-1): # recall that the last step in the subsequence has no ground truth (no next token!) so we don't train on it, TODO: use different Y technique to not waste training data
+                    # (B, H)
+                    x_t = X[i] # current time step input, i.e. i-th token in the sub-sequnce.... for every subsequence, shape (B, I), example: [embedding_of_34, embedding_of_77]
+
+                    # making a prediction for all sub-sequences ... all at once! and upadating hidden state for all subsequences
+                    # (B, H)      (B, O)
+                    hidden_state, output = self.forward(x_t, hidden_state=hidden_state)
+                    
+                    # every column of output represnets the logits of prediciion for the subsequence at that column, we softmax these logits
+                    # (B, O) shape didn't change, only each column is softmaxed
+                    output = torch.softmax(output, dim=1)
+
+                    loss = torch.mean(
+                        # (B, 1)
+                        -torch.log(   #(B, H)  (B, H)
+                            torch.sum(output * Y[i], dim=1, keepdim=True) + 1e-9 # notice this is pair-wise multiplication, not matrix dot product, 1e-9 for numerical stability
+                        )
+                    )
+
+                    epoch_loss += loss.item()
+
+                    self.optimizer.zero_grad()
+
+                    loss.backward()
+
+                    # avoiding exploding gradients
+                    torch.nn.utils.clip_grad_norm_(self.parameters, max_norm=max_grad_norm)
+
+                    self.optimizer.step()
+            
+            print(f"Epoch {epoch + 1} Loss: {epoch_loss / len(sequences)}")
+            if epoch + 1 in checkpoints:
+                models.append(self)
+                print(f"Model Saved @ Epoch {epoch + 1}")
+        
+        models.append(self)
+        return models
